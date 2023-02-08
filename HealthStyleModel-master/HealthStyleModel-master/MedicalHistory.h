6bc387946938c26be8d3e5f81ac4feda63c77e84
AlexRivera57 <a20190310@pucp.edu.pe>
2023-02-08 15:37:48 -0500

    .

--- HealthStyleModel-master/HealthStyleModel-master/MedicalHistory.h
+++ HealthStyleModel-master/HealthStyleModel-master/MedicalHistory.h
@@ -8,9 +8,15 @@ using namespace System;
 namespace HealthStyleModel {
     public ref class MedicalHistory {
     public:
+        property String^ Patient;
+        property int Age;
         property double Height;
         property double Weight;
-        property String^ Enfermedades;
-        property String^ Tratamientos;
+        property String^ BloodType;
+        property String^ PreviusDiseases;
+        property String^ CurrentsDiseases;
+        property String^ Medicines;
+        property String^ Treatmens;
+
     };
 }
