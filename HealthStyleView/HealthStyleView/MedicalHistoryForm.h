7f4c644b1bf5f4febe4277bef1f45e0182453590
AlexRivera57 <a20190310@pucp.edu.pe>
2023-02-08 15:34:55 -0500

    .

--- HealthStyleView/HealthStyleView/MedicalHistoryForm.h
+++ HealthStyleView/HealthStyleView/MedicalHistoryForm.h
@@ -41,6 +41,24 @@ namespace HealthStyleView {
 	private: System::Windows::Forms::Label^ label4;
 	private: System::Windows::Forms::Label^ label5;
 	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::TextBox^ textBox5;
+	private: System::Windows::Forms::TextBox^ textBox6;
+	private: System::Windows::Forms::Label^ label7;
+	private: System::Windows::Forms::Label^ label8;
+	private: System::Windows::Forms::TextBox^ textBox7;
+	private: System::Windows::Forms::TextBox^ textBox8;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Label^ label9;
+	private: System::Windows::Forms::TextBox^ textBox9;
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+
+
 
 	private:
 		/// <summary>
@@ -61,67 +79,229 @@ namespace HealthStyleView {
 			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->label5 = (gcnew System::Windows::Forms::Label());
 			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox6 = (gcnew System::Windows::Forms::TextBox());
+			this->label7 = (gcnew System::Windows::Forms::Label());
+			this->label8 = (gcnew System::Windows::Forms::Label());
+			this->textBox7 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox8 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->label9 = (gcnew System::Windows::Forms::Label());
+			this->textBox9 = (gcnew System::Windows::Forms::TextBox());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(54, 28);
+			this->label1->Location = System::Drawing::Point(358, 28);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(35, 13);
 			this->label1->TabIndex = 0;
-			this->label1->Text = L"label1";
+			this->label1->Text = L"Edad:";
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(57, 60);
+			this->label2->Location = System::Drawing::Point(205, 62);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(35, 13);
+			this->label2->Size = System::Drawing::Size(54, 13);
 			this->label2->TabIndex = 1;
-			this->label2->Text = L"label2";
+			this->label2->Text = L"Altura (m):";
 			// 
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(57, 97);
+			this->label3->Location = System::Drawing::Point(358, 62);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(35, 13);
+			this->label3->Size = System::Drawing::Size(55, 13);
 			this->label3->TabIndex = 2;
-			this->label3->Text = L"label3";
+			this->label3->Text = L"Peso (kg):";
 			// 
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(57, 130);
+			this->label4->Location = System::Drawing::Point(35, 62);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(35, 13);
+			this->label4->Size = System::Drawing::Size(83, 13);
 			this->label4->TabIndex = 3;
-			this->label4->Text = L"label4";
+			this->label4->Text = L"Tipo de Sangre:";
 			// 
 			// label5
 			// 
 			this->label5->AutoSize = true;
-			this->label5->Location = System::Drawing::Point(57, 160);
+			this->label5->Location = System::Drawing::Point(35, 89);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(35, 13);
+			this->label5->Size = System::Drawing::Size(127, 13);
 			this->label5->TabIndex = 4;
-			this->label5->Text = L"label5";
+			this->label5->Text = L"Enfermedades anteriores:";
 			// 
 			// label6
 			// 
 			this->label6->AutoSize = true;
-			this->label6->Location = System::Drawing::Point(60, 187);
+			this->label6->Location = System::Drawing::Point(35, 178);
 			this->label6->Name = L"label6";
-			this->label6->Size = System::Drawing::Size(35, 13);
+			this->label6->Size = System::Drawing::Size(121, 13);
 			this->label6->TabIndex = 5;
-			this->label6->Text = L"label6";
+			this->label6->Text = L"Enfermedades actuales:";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(419, 25);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(49, 20);
+			this->textBox1->TabIndex = 6;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(265, 59);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(48, 20);
+			this->textBox2->TabIndex = 7;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(419, 59);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(49, 20);
+			this->textBox3->TabIndex = 8;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(119, 59);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(37, 20);
+			this->textBox4->TabIndex = 9;
+			// 
+			// textBox5
+			// 
+			this->textBox5->Location = System::Drawing::Point(38, 105);
+			this->textBox5->Multiline = true;
+			this->textBox5->Name = L"textBox5";
+			this->textBox5->Size = System::Drawing::Size(430, 60);
+			this->textBox5->TabIndex = 10;
+			// 
+			// textBox6
+			// 
+			this->textBox6->Location = System::Drawing::Point(38, 194);
+			this->textBox6->Multiline = true;
+			this->textBox6->Name = L"textBox6";
+			this->textBox6->Size = System::Drawing::Size(430, 60);
+			this->textBox6->TabIndex = 11;
+			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->Location = System::Drawing::Point(35, 268);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(162, 13);
+			this->label7->TabIndex = 12;
+			this->label7->Text = L"Medicinas tomadas actualmente:";
+			// 
+			// label8
+			// 
+			this->label8->AutoSize = true;
+			this->label8->Location = System::Drawing::Point(35, 358);
+			this->label8->Name = L"label8";
+			this->label8->Size = System::Drawing::Size(68, 13);
+			this->label8->TabIndex = 13;
+			this->label8->Text = L"Tratamientos";
+			// 
+			// textBox7
+			// 
+			this->textBox7->Location = System::Drawing::Point(38, 284);
+			this->textBox7->Multiline = true;
+			this->textBox7->Name = L"textBox7";
+			this->textBox7->Size = System::Drawing::Size(430, 60);
+			this->textBox7->TabIndex = 14;
+			// 
+			// textBox8
+			// 
+			this->textBox8->Location = System::Drawing::Point(38, 374);
+			this->textBox8->Multiline = true;
+			this->textBox8->Name = L"textBox8";
+			this->textBox8->Size = System::Drawing::Size(430, 60);
+			this->textBox8->TabIndex = 15;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(81, 451);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 16;
+			this->button1->Text = L"Agregar";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(217, 451);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 17;
+			this->button2->Text = L"Modificar";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(350, 451);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(75, 23);
+			this->button3->TabIndex = 18;
+			this->button3->Text = L"Eliminar";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// label9
+			// 
+			this->label9->AutoSize = true;
+			this->label9->Location = System::Drawing::Point(35, 28);
+			this->label9->Name = L"label9";
+			this->label9->Size = System::Drawing::Size(52, 13);
+			this->label9->TabIndex = 19;
+			this->label9->Text = L"Paciente:";
+			// 
+			// textBox9
+			// 
+			this->textBox9->Location = System::Drawing::Point(93, 25);
+			this->textBox9->Name = L"textBox9";
+			this->textBox9->Size = System::Drawing::Size(247, 20);
+			this->textBox9->TabIndex = 20;
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Location = System::Drawing::Point(38, 495);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->Size = System::Drawing::Size(430, 104);
+			this->dataGridView1->TabIndex = 21;
 			// 
 			// MedicalHistoryForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(445, 429);
+			this->ClientSize = System::Drawing::Size(508, 611);
+			this->Controls->Add(this->dataGridView1);
+			this->Controls->Add(this->textBox9);
+			this->Controls->Add(this->label9);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox8);
+			this->Controls->Add(this->textBox7);
+			this->Controls->Add(this->label8);
+			this->Controls->Add(this->label7);
+			this->Controls->Add(this->textBox6);
+			this->Controls->Add(this->textBox5);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->label6);
 			this->Controls->Add(this->label5);
 			this->Controls->Add(this->label4);
@@ -130,6 +310,7 @@ namespace HealthStyleView {
 			this->Controls->Add(this->label1);
 			this->Name = L"MedicalHistoryForm";
 			this->Text = L"MedicalHistoryForm";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
