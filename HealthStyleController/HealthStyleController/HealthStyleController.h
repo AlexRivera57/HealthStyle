074c595d4b1fa153a2d5014189a3caa0f5e0f7c6
willmeza <123610565+willmeza@users.noreply.github.com>
2023-02-08 17:03:15 -0500

    Update HealthStyleController.h

--- HealthStyleController/HealthStyleController/HealthStyleController.h
+++ HealthStyleController/HealthStyleController/HealthStyleController.h
@@ -11,11 +11,20 @@ namespace HealthStyleController {
 		static List<Patient^>^ patientList = gcnew List<Patient^>();
 		static List<Medicines^>^ medicinesList = gcnew List<Medicines^>();
 		static List <Quote^>^ salesList = gcnew List<Quote^>(); // Lista de ventas registradas
+		static List<Historial^>^ historialList = gcnew List<Historial^>(); // Variable de clase (global) para la lista de historiales
 	public:
+		static void Init();
 		static int AddDoctor(Doctor^ doctor);
 		static List<Doctor^>^ QueryAllDoctor();
 		static int UpdateDoctor(Doctor^ doctor);
 		static int DeleteDoctor(int doctorId);
 		static Doctor^ QueryDoctorById(int doctorId);
+		
+		//Métodos CRUD de Historial Médico (C:Create, R:Request, U:Update, D:Delete)
+		static int AddHistorial(Historial^ historial);
+		//static Historial^ QueryProductById(int historialId);
+		static List<Historial^>^ QueryAllHistorial();
+		static int UpdateHistorial(Historial^ historial);
+		static int DeleteHistorial(int historial);
 	};
 }
